a34f92ce82d9712bcbe350c634f7d03f65ec31c5
Ahmed Alnakhif <ahmed.alnakhif@gmail.com>
2022-01-19 19:27:34 -0500

    implement valid parentheses (#15)

---
+++ src/Problems/StringAndArrayManipulation/ValidParentheses.java
@@ -0,0 +1,54 @@
+package Problems.StringAndArrayManipulation;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Example 1:
+ * Input: s = "()[]{}"
+ * Output: true
+ * 
+ * Example 2:
+ * Input: s = "(]"
+ * Output: false
+ */
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for (Character c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || map.get(c) != stack.pop()) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public void run() {
+        String s = "[([])]";
+
+        System.out.println(isValid(s));
+    }
+}
