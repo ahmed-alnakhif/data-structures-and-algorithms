7bf21771d5cd38c6a8f6bf612a2217b05f704393
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-01-22 12:30:55 -0500

    test

--- src/Problems/StringAndArrayManipulation/GroupAnagram.java
+++ src/Problems/StringAndArrayManipulation/GroupAnagram.java
@@ -7,31 +7,29 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Given two strings s and t, return true if t is an anagram of s, and false
- * otherwise.
+ * Given an array of strings strs, group the anagrams together. You can return
+ * the answer in any order.
  * 
  * An Anagram is a word or phrase formed by rearranging the letters of a
  * different word or phrase, typically using all the original letters exactly
  * once.
  * 
  * 
+ * 
  * Example 1:
  * 
- * Input: s = "anagram", t = "nagaram"
- * Output: true
- * Example 2:
- * 
- * Input: s = "rat", t = "car"
- * Output: false
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
  * 
  */
 
 public class GroupAnagram {
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0)
+        if (strs.length == 0) {
             return new ArrayList();
-
+        }
+        
         Map<String, List<String>> map = new HashMap<>();
 
         for (String str : strs) {
