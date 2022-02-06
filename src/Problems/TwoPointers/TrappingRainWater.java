ecfcd24a4d53a08cbc1b100f0849f3ee04bceee6
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-06 13:35:55 -0500

    LC problems

---
+++ src/Problems/TwoPointers/TrappingRainWater.java
@@ -0,0 +1,46 @@
+package Problems.TwoPointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given n non-negative integers representing an elevation map where the width
+ * of each bar is 1,
+ * compute how much water it can trap after raining.
+ * 
+ * Ex:
+ * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ * 
+ * Explanation: The above elevation map (black section) is represented by array
+ * [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section)
+ * are being trapped.
+ */
+
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        if (height.length == 0) return 0;
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int ans = 0;
+
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if (leftMax < rightMax) {
+                ans += Math.max(0, leftMax - height[left++]);
+            } else {
+                ans += Math.max(0, rightMax - height[right--]);
+            }
+        }
+
+        return ans;
+    }
+
+    public void run() {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height));
+    }
+}
