b09d765ae6dc310f1f1a13d155eb3d63fab40885
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-18 16:06:59 -0500

    implement LC

---
+++ src/Problems/TreeBreadthFirstSearch/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,65 @@
+package Problems.TreeBreadthFirstSearch;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given the root of a binary tree, return the average value of the nodes on
+ * each level in the form of an array. Answers within 10-5 of the actual answer
+ * will be accepted.
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [3.00000,14.50000,11.00000]
+ * Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5,
+ * and on level 2 is 11.
+ * Hence return [3, 14.5, 11].
+ */
+
+public class AverageOfLevelsInBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root == null) return result;
+
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Double sum = 0.0, count = 0.0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+                sum += node.val;
+                count++;
+            }
+            result.add(sum / count);
+        }
+
+        return result;
+    }
+
+    public void run() {
+
+    }
+}
