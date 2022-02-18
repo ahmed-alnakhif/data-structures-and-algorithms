9fc84c845f724980bd92450e3053ad4ee2faf2b0
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-18 16:57:47 -0500

    implement LC

---
+++ src/Problems/TreeBreadthFirstSearch/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,59 @@
+package Problems.TreeBreadthFirstSearch;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ * 
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ */
+
+public class MaximumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int minDepth(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root == null) return 0;
+
+        int minDepth = 0;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            minDepth++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+
+        return minDepth;
+    }
+
+    public void run() {
+
+    }
+}
