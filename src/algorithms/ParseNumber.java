da05f84c1ba8a5a0cf76b69e8cf8aec304bf238e
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-24 09:35:56 -0500

    refactor

---
+++ src/algorithms/ParseNumber.java
@@ -0,0 +1,47 @@
+package algorithms;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ParseNumber {
+
+    List<Integer> parseInteger(int num) {
+        LinkedList<Integer> result = new LinkedList<>();
+
+        while (num > 1) {
+            int digit = num % 10;
+            num = num / 10;
+            result.addFirst(digit);
+        }
+
+        return result;
+    }
+
+    //Not working!! 
+    List<List<Integer>> parseDecimal(double num) {
+        List<List<Integer>> result = new LinkedList<>();
+        LinkedList<Integer> integersList = new LinkedList<>();
+        LinkedList<Integer> decimalList = new LinkedList<>();
+
+        while (num > 1) {
+            double digit = num % 10;
+            num = num / 10;
+            integersList.addFirst((int)digit);
+        }
+        result.add(integersList);
+
+        System.out.println(num);
+        while(num != 0){
+            int tmp = (int)(num * 10);
+            num = num - tmp;
+        }
+
+
+        return result;
+    }
+
+    public void run() {
+        int num = 54321;
+        System.out.println(parseDecimal(num));
+    }
+}
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,10 +1,12 @@
 package com.main;
 
+import algorithms.ParseNumber;
 
 public class Main {
 
     public static void main(String[] args) {
         // call <Class_Instance>.run() here
-        
+        ParseNumber parseNumber = new ParseNumber();
+        parseNumber.run();
     }
 }
