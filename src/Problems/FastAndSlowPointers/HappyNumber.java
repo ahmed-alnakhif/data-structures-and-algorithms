b2d3764c2803ef6c9343d90f41e046e9488f90a8
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-24 08:15:48 -0500

    refactor

--- src/Problems/FastAndSlowPointers/HappyNumber.java
+++ src/Problems/FastAndSlowPointers/HappyNumber.java
@@ -1,20 +1,30 @@
 package Problems.FastAndSlowPointers;
 
 /**
- * Given an array of integers nums containing n + 1 integers where each integer
- * is in the range [1, n] inclusive.
+ * Write an algorithm to determine if a number n is happy.
  * 
- * There is only one repeated number in nums, return this repeated number.
+ * A happy number is a number defined by the following process:
  * 
- * You must solve the problem without modifying the array nums and uses only
- * constant extra space.
- * 
- * 
+ * Starting with any positive integer, replace the number by the sum of the
+ * squares of its digits.
+ * Repeat the process until the number equals 1 (where it will stay), or it
+ * loops endlessly in a cycle which does not include 1.
+ * Those numbers for which this process ends in 1 are happy.
+ * Return true if n is a happy number, and false if not.
  * 
  * Example 1:
  * 
- * Input: nums = [1,3,4,2,2]
- * Output: 2
+ * Input: n = 19
+ * Output: true
+ * Explanation:
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * Example 2:
+ * 
+ * Input: n = 2
+ * Output: false
  */
 
 public class HappyNumber {
@@ -24,7 +34,7 @@ public class HappyNumber {
         int slow = n;
         int fast = getNextDigit(n);
 
-        while (slow != fast && fast != 1) {
+        while (fast != 1 && slow != fast) {
             slow = getNextDigit(slow);
             fast = getNextDigit(getNextDigit(fast));
         }
