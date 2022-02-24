7e59a7b577d73389e837f283a87361626e2e77f5
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-23 22:53:21 -0500

    implement LC

--- src/algorithms/Combination.java
+++ src/algorithms/Combination.java
@@ -1,28 +1,51 @@
 package algorithms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
+
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out
+ * of the range [1, n].
+ * 
+ * You may return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: n = 4, k = 2
+ * Output:
+ * [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4] ]
+ */
 
 public class Combination {
-    public static class Solution {
-        List<List<Integer>> result = new ArrayList<>();
+    List<List<Integer>> result = new LinkedList<>();
 
-        private void swap(int[] arr, int i, int j){
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
+    public List<List<Integer>> combine(int n, int k) {
+        combination(1, n, k, new LinkedList<>());
+        return result;
+    }
+
+    private void combination(int start, int n, int k, LinkedList<Integer> comb) {
+        if (comb.size() == k) {
+            result.add(new LinkedList<>(comb));
+            return;
         }
 
-        private List<List<Integer>> combination(int[] nums){
-
-            return result;
+        // optimization -> return if there aren't enough numbers left to fill the
+        // partial comb list up to k
+        if (k - comb.size() > n - start + 1) {
+            return;
         }
 
-        public void main(){
-            int[] nums = {1,2,3};
-            System.out.println(combination(nums));
+        for (int i = start; i < n + 1; i++) {
+            comb.add(i);
+            combination(i + 1, n, k, comb);
+            comb.removeLast();
         }
+    }
+
+    public void run() {
+        int n = 4, k = 2;
+        System.out.println(combine(n, k));
     }
 }
