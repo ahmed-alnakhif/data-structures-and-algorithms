b4af2ad7ce4f54ebca6a887a5a845778c45cbfe8
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-02-24 00:19:06 -0500

    implement LC

--- src/Problems/SlidingWindow/FruitIntoBaskets.java
+++ src/Problems/SlidingWindow/FruitIntoBaskets.java
@@ -4,13 +4,29 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Given an integer array nums, return true if any value appears at least twice
- * in the array, and return false if every element is distinct.
+ * You are visiting a farm that has a single row of fruit trees arranged from
+ * left to right. The trees are represented by an integer array fruits where
+ * fruits[i] is the type of fruit the ith tree produces.
+ * 
+ * You want to collect as much fruit as possible. However, the owner has some
+ * strict rules that you must follow:
+ * 
+ * You only have two baskets, and each basket can only hold a single type of
+ * fruit. There is no limit on the amount of fruit each basket can hold.
+ * Starting from any tree of your choice, you must pick exactly one fruit from
+ * every tree (including the start tree) while moving to the right. The picked
+ * fruits must fit in one of your baskets.
+ * Once you reach a tree with fruit that cannot fit in your baskets, you must
+ * stop.
+ * Given the integer array fruits, return the maximum number of fruits you can
+ * pick.
+ * 
  * 
  * Example 1:
  * 
- * Input: nums = [1,2,3,1]
- * Output: true
+ * Input: fruits = [1,2,1]
+ * Output: 3
+ * Explanation: We can pick from all 3 trees.
  */
 
 public class FruitIntoBaskets {
