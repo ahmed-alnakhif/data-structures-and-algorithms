240df10e7be49be2436fc17c75282580203f18f6
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-03-01 18:47:25 -0500

    implement dp

--- src/Problems/dynamicProgramming/tabulation/CanSum.java
+++ src/Problems/dynamicProgramming/tabulation/CanSum.java
@@ -1,52 +1,33 @@
 package Problems.dynamicProgramming.tabulation;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /*
  return true if 2 numbers in the array can sum up to the target number
 */
 
 public class CanSum {
 
-    Map<Integer, Boolean> memoMap = new HashMap<>();
+    static boolean canSum(int targetSum, int[] nums) {
+        boolean[] table = new boolean[targetSum + 1];
+        table[0] = true;
 
-    boolean targetSum(int target, int[] nums) {
-        boolean result = canSum(target, nums);
-        memoMap.clear();
-        return result;
-    }
-
-    boolean canSum(int target, int[] nums) {
-        if (memoMap.containsKey(target)) {
-            return memoMap.get(target);
-        }
-
-        if (target == 0) {
-            return true;
-        }
-
-        if (target < 0) {
-            return false;
-        }
-
-        for (int num : nums) {
-            int remainder = target - num;
-            if (canSum(remainder, nums)) {
-                memoMap.put(target, true);
-                return true;
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] == true) {
+                for (int num : nums) {
+                    if (i + num < table.length) {
+                        table[i + num] = true;
+                    }
+                }
             }
         }
 
-        memoMap.put(target, false);
-        return false;
+        return table[targetSum];
     }
 
-    public void run() {
-        System.out.println(targetSum(7, new int[] { 2, 3 })); // true
-        System.out.println(targetSum(7, new int[] { 2, 3, 4, 7 })); // true
-        System.out.println(targetSum(7, new int[] { 2, 4 })); // false
-        System.out.println(targetSum(8, new int[] { 2, 3, 5 })); // true
-        System.out.println(targetSum(300, new int[] { 7, 14 })); // false
+    public static void main(String[] args) {
+        System.out.println(canSum(7, new int[] { 2, 3 })); // true
+        System.out.println(canSum(7, new int[] { 2, 3, 4, 7 })); // true
+        System.out.println(canSum(7, new int[] { 2, 4 })); // false
+        System.out.println(canSum(8, new int[] { 2, 3, 5 })); // true
+        System.out.println(canSum(300, new int[] { 7, 14 })); // false
     }
 }
