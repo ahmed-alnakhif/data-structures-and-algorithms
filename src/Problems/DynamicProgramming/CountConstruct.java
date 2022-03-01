8feccc605af4ac8cf5f7241b91612f55908171ca
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-03-01 09:54:48 -0500

    implement dp

--- src/Problems/DynamicProgramming/CountConstruct.java
+++ src/Problems/DynamicProgramming/CountConstruct.java
@@ -9,22 +9,43 @@ import java.util.Map;
 
 public class CountConstruct {
 
-    static Map<String, Boolean> memoMap;
+    static Map<String, Integer> memoMap;
 
-    static Boolean countConstruct(String word, String[] wordBank) {
+    static int countConstruct(String word, String[] wordBank) {
         memoMap = new HashMap<>();
         return countConstructHelper(word, wordBank);
     }
 
-    static Boolean countConstructHelper(String word, String[] wordBank) {
-        
+    static int countConstructHelper(String target, String[] wordBank) {
+        if (target.equals("")) {
+            return 1;
+        }
+
+        if (memoMap.containsKey(target)) {
+            return memoMap.get(target);
+        }
+
+        int totalCount = 0;
+        for (String word : wordBank) {
+            if (target.indexOf(word) == 0) {
+                String suffix = target.substring(word.length(), target.length());
+                totalCount += countConstructHelper(suffix, wordBank);
+            }
+        }
+
+        memoMap.put(target, totalCount);
+
+        return totalCount;
     }
 
     public static void main(String[] args) {
-        System.out.println(countConstruct("abcdef", new String[] { "ab", "abc", "cd", "def", "abcd" })); 
-        System.out.println(countConstruct("skateboard", new String[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" })); 
-        System.out.println(countConstruct("enterapotentpot", new String[] { "a", "p", "ent", "enter", "ot", "o", "t" })); 
+        System.out.println(countConstruct("abcdef", new String[] { "ab", "abc", "cd", "def", "abcd" }));
+        System.out.println(countConstruct("skateboard", new String[] { "bo", "rd", "ate", "t", "ska", "sk", "boar" }));
+        System.out
+                .println(countConstruct("enterapotentpot", new String[] { "a", "p", "ent", "enter", "ot", "o", "t" }));
+        System.out.println(countConstruct("purple", new String[] { "purp", "p", "ur", "le", "purpl" }));
         System.out.println(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
-                new String[] { "e", "ee", "eee", "eeee", "eeeee" })); 
+                new String[] { "e", "ee", "eee", "eeee", "eeeee" }));
     }
 }
+ 
