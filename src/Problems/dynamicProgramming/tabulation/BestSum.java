e8afff541db5d3eaa0ea37d6ea67180ec1b4f71a
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-03-01 21:44:18 -0500

    implement dp

--- src/Problems/dynamicProgramming/tabulation/BestSum.java
+++ src/Problems/dynamicProgramming/tabulation/BestSum.java
@@ -1,26 +1,40 @@
 package Problems.dynamicProgramming.tabulation;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /*
- return list of the shortest combination in the array that add up to the target number
+ return list of the smallest combination in the array that add up to the target number
 */
 
 public class BestSum {
 
     static List<Integer> bestSum(int target, int[] nums) {
-        
-    }
+        List[] table = new List[target + 1];
+        table[0] = new ArrayList<>();
 
-  
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                for (int num : nums) {
+                    if (i + num < table.length) {
+                        ArrayList<Integer> newList = new ArrayList<>(table[i]);
+                        newList.add(num);
+                        if (table[i + num] == null || newList.size() < table[i + num].size()) {
+                            table[i + num] = newList;
+                        }
+                    }
+                }
+            }
+        }
+
+        return table[target];
+    }
 
     public static void main(String[] args) {
         System.out.println(bestSum(7, new int[] { 5, 3, 4, 7 }));
         System.out.println(bestSum(8, new int[] { 2, 3, 5 }));
         System.out.println(bestSum(8, new int[] { 1, 4, 5 }));
-        System.out.println(bestSum(100, new int[] { 1, 2, 5, 25 })); // not correct
+        System.out.println(bestSum(100, new int[] { 1, 2, 5, 25 }));
+        System.out.println(bestSum(100, new int[] { 25, 1, 2, 5 }));
     }
 }
