ac603898510bc34e6cb703111447b4f34c7f63fa
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-03-06 18:07:41 -0500

    implement bs

--- src/Problems/modifiedBinarySearch/BinarySearch.java
+++ src/Problems/modifiedBinarySearch/BinarySearch.java
@@ -20,7 +20,7 @@ public class BinarySearch {
     public static int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + ((right - left) / 2);
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
