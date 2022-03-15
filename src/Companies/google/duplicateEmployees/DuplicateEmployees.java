e2c78930310c0b621cc12a7ad36eb57f93dcd4c9
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-03-15 13:59:34 -0400

    imp: read csv

---
+++ src/Companies/google/duplicateEmployees/DuplicateEmployees.java
@@ -0,0 +1,86 @@
+package Companies.google.duplicateEmployees;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DuplicateEmployees {
+
+    private List<String> getDuplicateEmployees(String filePath) {
+        List<List<String>> employees = getEmployees(filePath);
+        HashMap<String, String> employeesMap = new HashMap<>();
+        Set<String> duplicateEmployees = new HashSet<>();
+
+        for (List<String> employeeInfo : employees) {
+            String name = employeeInfo.get(0);
+            String id = employeeInfo.get(1);
+            if (employeesMap.containsKey(id)) {
+                duplicateEmployees.add(name);
+                duplicateEmployees.add(employeesMap.get(id));
+            } else {
+                employeesMap.put(id, name);
+            }
+        }
+
+        return new ArrayList<>(duplicateEmployees);
+    }
+
+    private List<List<String>> getEmployees(String filePath) {
+        List<List<String>> records = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            while (scanner.hasNext()) {
+                records.add(getRecordFromLine(scanner.nextLine()));
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error while opening " + filePath + ", " + e);
+        }
+
+        return records;
+    }
+
+    private List<String> getRecordFromLine(String line) {
+        List<String> record = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(",");
+            while (rowScanner.hasNext()) {
+                record.add(rowScanner.next());
+            }
+        }
+        return record;
+    }
+
+    public static List<String> duplicateEmployees(String file) {
+        HashSet<String> duplicateEmployeesSet = new HashSet<>();
+        HashMap<String, String> employeesMap = new HashMap<>();
+
+        // try {
+        //     CSVReader reader = new CSVReaderBuilder(new FileReader(file)).build();
+
+        //     reader.readAll().stream().forEach((record) -> {
+        //         if (employeesMap.containsKey(record[1])) {
+        //             duplicateEmployeesSet.add(record[0]);
+        //             duplicateEmployeesSet.add(employeesMap.get(record[1]));
+        //         }
+
+        //         employeesMap.put(record[1], record[0]);
+        //     });
+        // } catch (Exception e) {
+        //     System.out.println(e);
+        // }
+
+        return new ArrayList<>(duplicateEmployeesSet);
+    }
+
+    public static void main(String[] args) {
+        DuplicateEmployees dEmployees = new DuplicateEmployees();
+        System.out.println(dEmployees.getDuplicateEmployees("src/Companies/google/duplicateEmployees/employees.csv"));
+    }
+}
---
+++ src/Companies/google/duplicateEmployees/Employee.java
@@ -0,0 +1,41 @@
+package Companies.google.duplicateEmployees; 
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private String description;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
