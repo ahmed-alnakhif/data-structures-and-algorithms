59faf1a50fa82811435212f9be439db75d4c6353
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-04-18 13:45:55 -0400

    refactor

--- src/Problems/hashMap/UserActiveMinutes.java
+++ src/Problems/hashMap/UserActiveMinutes.java
@@ -44,14 +44,12 @@ public class UserActiveMinutes {
         Map<Integer, Set<Integer>> map = new HashMap<>();
 
         for (int[] log : logs) {
-            if (!map.containsKey(log[0])) {
-                map.put(log[0], new HashSet<>());
-            }
+            map.putIfAbsent(log[0], new HashSet<>());
             map.get(log[0]).add(log[1]);
         }
 
         for (Set<Integer> set : map.values()) {
-            uam[set.size() - 1]++;
+            uam[set.size()-1]++;
         }
 
         return uam;
--- src/Problems/kWayMerge/FindKLargestPairs.java
+++ src/Problems/kWayMerge/FindKLargestPairs.java
@@ -6,32 +6,37 @@ import java.util.PriorityQueue;
 
 public class FindKLargestPairs {
 
-    public static List<List<Integer>> kLargestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<List<Integer>> minHeap = new PriorityQueue<>(
-                (a, b) -> (a.get(0) + a.get(1)) - (b.get(0) + b.get(1)));
-
+    public static List<int[]> kLargestPairs(int[] nums1, int[] nums2, int k) {
+        List<int[]> result = new ArrayList<>();
+        if (nums1.length * nums2.length * k == 0) {
+            return result;
+        }
+    
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> (nums1[b[0]] + nums2[b[1]]) - (nums1[a[0]] + nums2[a[1]]));
+    
         for (int i = 0; i < Math.min(nums1.length, k); i++) {
-            for (int j = 0; j < Math.min(nums1.length, k); j++) {
-                if (minHeap.size() < k) {
-                    minHeap.add(new ArrayList<>(List.of(nums1[i], nums2[j])));
-                } else {
-                    if (nums1[i] + nums2[j] < minHeap.peek().get(0) + minHeap.peek().get(1)) {
-                        break;
-                    } else {
-                        minHeap.poll();
-                        minHeap.add(new ArrayList<>(List.of(nums1[i], nums2[j])));
-                    }
-                }
+            maxHeap.add(new int[] { i, 0 });
+        }
+    
+        while (!maxHeap.isEmpty() && k-- > 0) {
+            int[] curr = maxHeap.poll();
+    
+            result.add(new int[] { nums1[curr[0]], nums2[curr[1]]});
+    
+            if (curr[1] < nums2.length - 1) {
+                curr[1]++;
+                maxHeap.add(curr);
             }
         }
-
-        return new ArrayList<>(minHeap);
-    }
+    
+        return result;
+      }
 
     public static void main(String[] args) {
         int[] nums1 = { 9, 8, 2 };
         int[] nums2 = { 6, 3, 1 };
         int k = 3;
-        System.out.println(kLargestPairs(nums1, nums2, k));
+        System.out.println(kLargestPairs(nums1, nums2, k)); //[9, 6] [8, 6] [9, 3] 
     }
 }
