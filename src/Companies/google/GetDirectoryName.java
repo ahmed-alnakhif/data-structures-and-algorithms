722f8ce79aca57026c9692c79e25d622ba55dddc
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-05-10 10:44:59 -0400

    imp: google

--- src/Companies/google/GetDirectoryName.java
+++ src/Companies/google/GetDirectoryName.java
@@ -1,5 +1,29 @@
 package Companies.google;
 
+import java.util.List;
+
+/**
+ * convert list of folders to list of gmail labels 
+ */
+class Folder {
+    int id;
+    int parentId;
+    String name;
+
+    Folder(int id, int parentId, String name) {
+        this.id = id;
+        this.parentId = parentId;
+        this.name = name;
+    }
+}
+
 public class GetDirectoryName {
     
+    public void getDirectoryName(List<Folder> folders) {
+       
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
