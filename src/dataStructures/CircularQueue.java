913dae1bd0299e34c7614c10efa9d73e3f5ef513
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-05-27 23:54:21 -0400

    imp circular queue

--- src/dataStructures/CircularQueue.java
+++ src/dataStructures/CircularQueue.java
@@ -1,7 +1,60 @@
 package dataStructures;
 
+import java.util.LinkedList;
+
 public class CircularQueue {
+
+    LinkedList<Integer> list;
+    int size;
+
+    public CircularQueue(int k) {
+        list = new LinkedList<>();
+        size = k;
+    }
+
+    public boolean enQueue(int value) {
+        if (list.size() >= size) {
+            return false;
+        }
+
+        list.addLast(value);
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (list.isEmpty()) {
+            return false;
+        }
+
+        list.removeFirst();
+        return true;
+    }
+
+    public int Front() {
+        if (list.isEmpty()) {
+            return -1;
+        } else {
+            return list.getFirst();
+        }
+    }
+
+    public int Rear() {
+        if (list.isEmpty()) {
+            return -1;
+        } else {
+            return list.getLast();
+        }
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean isFull() {
+        return list.size() == size;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
