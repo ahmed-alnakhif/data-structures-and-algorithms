c3ce162823ff4e0d3afc2de0b2b88c63f28de9b5
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-06-05 09:35:01 -0400

    sliding window

--- src/Problems/SlidingWindow/MaxConsecutiveOnesII.java
+++ src/Problems/SlidingWindow/MaxConsecutiveOnesII.java
@@ -4,7 +4,6 @@ package Problems.SlidingWindow;
  * Given a binary array nums, return the maximum number of consecutive 1's in
  * the array if you can flip at most one 0.
  * 
- * 
  * Example 1:
  * 
  * Input: nums = [1,0,1,1,0]
