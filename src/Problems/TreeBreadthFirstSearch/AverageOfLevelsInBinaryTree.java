492cb18c90b99831824e5f9234499260d8620c22
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-06-09 07:53:12 -0400

    tree BFS

--- src/Problems/TreeBreadthFirstSearch/AverageOfLevelsInBinaryTree.java
+++ src/Problems/TreeBreadthFirstSearch/AverageOfLevelsInBinaryTree.java
@@ -46,12 +46,14 @@ public class AverageOfLevelsInBinaryTree {
         while (!queue.isEmpty()) {
             Double sum = 0.0;
             int size = queue.size();
+
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 if (node.left != null) queue.add(node.left);
                 if (node.right != null) queue.add(node.right);
                 sum += node.val;
             }
+            
             result.add(sum / size);
         }
 
