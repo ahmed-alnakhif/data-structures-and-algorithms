eb7f42c1eb62ad25d7ff850f643d535862ffcb2d
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-06-08 23:40:54 -0400

    merge intervals

--- src/Problems/Intervals/CarPooling.java
+++ src/Problems/Intervals/CarPooling.java
@@ -2,14 +2,36 @@ package Problems.Intervals;
 
 import java.util.TreeMap;
 
+/**
+ * There is a car with capacity empty seats. The vehicle only drives east (i.e.,
+ * it cannot turn around and drive west).
+ * 
+ * You are given the integer capacity and an array trips where trips[i] =
+ * [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi
+ * passengers and the locations to pick them up and drop them off are fromi and
+ * toi respectively. The locations are given as the number of kilometers due
+ * east from the car's initial location.
+ * 
+ * Return true if it is possible to pick up and drop off all passengers for all
+ * the given trips, or false otherwise.
+ * 
+ * Example 1:
+ * 
+ * Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+ * Output: false
+ * Example 2:
+ * 
+ * Input: trips = [[2,1,5],[3,3,7]], capacity = 5
+ * Output: true
+ */
+
 public class CarPooling {
 
     // T: O(N*log(N)), S: O(N)
     public boolean carPooling(int[][] trips, int capacity) {
-        //if values are the same, pick drop off time first
-        // PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->
-        //     a[0] == b[0] ? b[2]-a[2]: a[0]-b[0] 
-        // );
+        // if values are the same, pick drop off time first
+        // PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)-> a[0] == b[0] ? b[2]-a[2]: a[0]-b[0]);
+
         TreeMap<Integer, Integer> timestamp = new TreeMap<>();
 
         for (int[] trip : trips) {
@@ -47,7 +69,7 @@ public class CarPooling {
     public static void main(String[] args) {
         CarPooling cp = new CarPooling();
         int[][] trips = new int[][] {
-            { 2, 1, 5 }, { 3, 3, 7 }
+                { 2, 1, 5 }, { 3, 3, 7 }
         };
         System.out.println(cp.carPooling(trips, 4));
     }
