2f82ba11408276e33d71bb8f323a85526f726179
ahmed-alnakhif <ahmed.alnakhif@gmail.com>
2022-06-10 05:08:00 -0400

    graphs

--- src/Problems/Graphs/ReconstructItinerary.java
+++ src/Problems/Graphs/ReconstructItinerary.java
@@ -8,7 +8,28 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * return the number of nodes in the largest connected component in a graph
+ * You are given a list of airline tickets where tickets[i] = [fromi, toi]
+ * represent the departure and the arrival airports of one flight. Reconstruct
+ * the itinerary in order and return it.
+ * 
+ * All of the tickets belong to a man who departs from "JFK", thus, the
+ * itinerary must begin with "JFK". If there are multiple valid itineraries, you
+ * should return the itinerary that has the smallest lexical order when read as
+ * a single string.
+ * 
+ * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than
+ * ["JFK", "LGB"].
+ * You may assume all tickets form at least one valid itinerary. You must use
+ * all the tickets once and only once
+ * 
+ * Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+ * Output: ["JFK","MUC","LHR","SFO","SJC"]
+ * 
+ * Input: tickets =
+ * [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * Explanation: Another possible reconstruction is
+ * ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.
  */
 
 public class ReconstructItinerary {
@@ -24,6 +45,7 @@ public class ReconstructItinerary {
     private void dfs(String src) {
         if (flightsMap.containsKey(src)) {
             LinkedList<String> destList = flightsMap.get(src);
+            
             while (!destList.isEmpty()) {
                 dfs(destList.pollFirst());
             }
